8b870382f151c5f1374e7d40fba08c148ec44722
madgrv <mad.grv@gmail.com>
2023-06-30 14:27:24 +0100

    Created using create-react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import styled from "styled-components";
+
+function App() {
+  return (
+    <div className="App">
+      Hello World! Here will live my To Do list - App!
+    </div>
+  );
+}
+
+export default App;
