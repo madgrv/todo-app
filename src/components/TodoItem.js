a0da952999b73e063a0a6f5c49eb98393fcb3128
madgrv <mad.grv@gmail.com>
2023-07-01 17:01:20 +0100

    Created the todo item component with css module

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect} from 'react';
+import styles from '../styles/todo-item.module.css';
 
 const TodoItem = (props) => {
     return(
-        <div>
-            
+        <div className={styles.card}>
+            <input type="checkbox" checked={props.completed} />
+            <span>{props.title}</span>
+            <button>Edit</button>
+            <button>Delete</button>
         </div>
     )
 }
